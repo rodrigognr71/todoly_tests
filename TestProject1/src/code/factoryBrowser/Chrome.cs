5d808839e16dd1042722188a09fa06206a53cf9d
rodrigognr71 <44789723+rodrigognr71@users.noreply.github.com>
2023-06-09 20:58:31 -0400

    Update Chrome.cs

--- TestProject1/src/code/factoryBrowser/Chrome.cs
+++ TestProject1/src/code/factoryBrowser/Chrome.cs
@@ -14,7 +14,7 @@ namespace TestProject1.src.code.factoryBrowser
         {           
             Console.WriteLine("setup");
             string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-            IWebDriver driver = new ChromeDriver(path + "resources/driver");
+            IWebDriver driver = new ChromeDriver(path + "/resources/driver");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Manage().Window.Maximize();
             return driver;
